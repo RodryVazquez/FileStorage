8e809d65025732d1badbe58bab04a647ff1416fa
Rodrigo Vazquez <rodrigo@rodrigovazquez.com.mx>
2017-05-11 09:54:11 -0600

    Se agrego clase Helper para la BD y contenido en SQLStorage

--- app/src/main/java/com/example/rodrigovazquez/filestorage/Activities/SQLStorage.java
+++ app/src/main/java/com/example/rodrigovazquez/filestorage/Activities/SQLStorage.java
@@ -3,13 +3,22 @@ package com.example.rodrigovazquez.filestorage.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.rodrigovazquez.filestorage.R;
+import com.example.rodrigovazquez.filestorage.Util.EventDataSQLHelper;
 
 /**
  *
  */
 public class SQLStorage extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText enterData;
+    private TextView showData;
+    private Button save, show;
+    EventDataSQLHelper userData;
 
     /**
      *
@@ -23,12 +32,55 @@ public class SQLStorage extends AppCompatActivity implements View.OnClickListene
 
     /**
      *
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * Escuchamos click de las views
      * @param v
      */
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-
+            case R.id.btnSave:
+                break;
+            case R.id.btnShow:
+                break;
         }
     }
 }
